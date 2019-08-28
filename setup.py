f81c49a7c4f9adb5f733ee39221f332cd3ba6d83
TA2 Experiment Team <>
2019-08-28 14:13:07 +0100

    Convert to a Python package using setuptools

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='ta2_hrr_2019',
+      version='0.0.0',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=True,
+      )
